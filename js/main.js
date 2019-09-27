cde9fc6ad79da8935d7bea94ee9a6dd1432e8151
Todd Peterson <ToddPetersonGV@gmail.com>
2019-09-26 22:20:26 -0600

    Highlight navbar links and add scroll animation

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ let lastPos = 0;
 
 $(window).on('scroll', () => {
     const pos = $(window).scrollTop();
-    const pos2 = pos + 50;
+    const pos2 = pos + 500;
     const scrollBottom = pos + $(window).height();
 
     if (pos >= navPos + $('nav').height() && lastPos < pos) {
@@ -12,5 +12,36 @@ $(window).on('scroll', () => {
     if (pos < navPos && lastPos > pos) {
         $('nav').removeClass('sticky');
     }
+
+    if (pos2 > $('#home').offset().top) {
+        highlightLink('home');
+    }
+    if (pos2 > $('#projects').offset().top) {
+        highlightLink('projects');
+    }
+    if (pos2 > $('#contact').offset().top) {
+        highlightLink('contact');
+    }
+
     lastPos = pos;
+});
+
+function highlightLink(anchor) {
+    $('nav .active').removeClass('active');
+    $('nav')
+        .find('[dest="' + anchor + '"]')
+        .addClass('active');
+}
+
+$('.nav-link').click(function() {
+    const anchor = $(this).attr('dest');
+    console.log("TCL: this", $(this))
+    console.log('TCL: anchor', anchor);
+
+    $('html, body').animate(
+        {
+            scrollTop: $('#' + anchor).offset().top
+        },
+        400
+    );
 });
